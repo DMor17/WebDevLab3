e6576a6c1c10e74af4b0104b2f57e2d6c3fb0522
Daniel <dannymorison17@Gmail.com>
2015-11-02 16:01:25 +0000

    Added loginSuccessful and cookie reading functionality

--- homepage.php
+++ homepage.php
@@ -6,10 +6,10 @@
 
 
 function displayAccessLevelInformation() {
-    if ($_COOKIE["access_level"] == "standarduser") {
+    if ($_COOKIE["access_level"] = "standarduser") {
         echo "<p>You are currently logged in as a standard user</p>";
     }
-    elseif ($_COOKIE["access_level"] == "root") {
+    elseif ($_COOKIE["access_level"] = "root") {
         echo "<p>You are currently logged in as a root user</p>";
         echo "<p>You now have access to additional administrative features</p>";
     }
