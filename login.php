a6f12c60b54c1de8b6fe3294b865295f2ab8f789
Daniel <dannymorison17@Gmail.com>
2015-11-02 15:40:32 +0000

    Added loginSuccessful and cookie reading functionality

--- login.php
+++ login.php
@@ -8,14 +8,16 @@
     Password:<input type="text" password="password"><br>
     <input type="submit" value="Submit">
 </form>
+
+
 <?php
 
  //   if (isset($_POST['submit'])) {
    //     if ($username = $_POST["username"] && $password = $_POST["password"]) {
      //       setcookie("username", $username, 75748, "", "");
        //     setcookie("access_level", "standarduser", 75748, "", "");
-            //header('Location: loginSuccessful.php');
-         //   header('Location: loginSuccessful.php');
+            //header('Location: homepage.php');
+         //   header('Location: homepage.php');
           //  exit;
       //  } else {
        //     echo "Wrong info";
