c54d3060d06032d076c199180ea7e9bf4263de7f
Daniel <dannymorison17@Gmail.com>
2015-11-02 15:21:16 +0000

    Added loginSuccessful and cookie reading functionality

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 
 
 
-<form method="post" action=<?php logInValidation()?>>
+<form method="post">
 
     Username:<input type="text" name="username"><br>
     Password:<input type="text" password="password"><br>
@@ -13,24 +13,23 @@
 
 
 
-function logInValidation(){
 
 
 
     $username = "Mike";
     $password = "password1";
-if($username = $_POST["username"] && $password = $_POST["password"])
-{
-    setcookie("username", $username, 75748,"","");
-    setcookie("access_level", "standarduser", 75748,"","");
-    //header('Location: loginSuccessful.php');
-    header('Location: http://1406997.azurewebsites.net/loginSuccessful.php');
-    exit;
-}
-    else{
-        echo "Wrong info";
+    if (isset($_POST['submit'])) {
+        if ($username = $_POST["username"] && $password = $_POST["password"]) {
+            setcookie("username", $username, 75748, "", "");
+            setcookie("access_level", "standarduser", 75748, "", "");
+            //header('Location: loginSuccessful.php');
+            header('Location: loginSuccessful.php');
+            exit;
+        } else {
+            echo "Wrong info";
 
-    }
+        }
+
 
 }
 ?>
