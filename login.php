619aadadac7bab60da41b00fa10e732ceee6dc38
Daniel <dannymorison17@Gmail.com>
2015-11-02 14:58:57 +0000

    Added loginSuccessful and cookie reading functionality

--- login.php
+++ login.php
@@ -19,7 +19,7 @@ function logInValidation(){
 
     $username = "Mike";
     $password = "password1";
-if($username = $_POST["username"] AND $password = $_POST["password"])
+if($username = $_POST["username"] && $password = $_POST["password"])
 {
     setcookie("username", $username, 75748,"","");
     setcookie("access_level", "standarduser", 75748,"","");
