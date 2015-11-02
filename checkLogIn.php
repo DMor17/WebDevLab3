2c6ccb3346824e5d7f424af02d2b885abbc6646d
Daniel <dannymorison17@Gmail.com>
2015-11-02 15:43:04 +0000

    Added loginSuccessful and cookie reading functionality

--- checkLogIn.php
+++ checkLogIn.php
@@ -3,11 +3,11 @@
 $username = "Mike";
 $password = "password1";
 
-$user = $_POST["username"];
-$pass = $_POST["password"];
 
 
-if ($username = $user & $password = $pass){
+
+
+if ($username = $_POST["username"] && $password = $_POST["password"]){
 
 setcookie("username", $username, 75748, "", "");
 setcookie("access_level", "standarduser", 75748, "", "");
