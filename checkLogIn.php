0b4da5f884bc1e6e4923434b6c24e29c58424f99
Daniel <dannymorison17@Gmail.com>
2015-11-02 15:51:53 +0000

    Added loginSuccessful and cookie reading functionality

--- checkLogIn.php
+++ checkLogIn.php
@@ -1,11 +1,7 @@
 <?php
 
 $username = "Mike";
-$password = "password1";
-
-
-
-
+$password = "password";
 
 if ($username = $_POST["username"] && $password = $_POST["password"]){
 
@@ -13,6 +9,7 @@ setcookie("username", $username, 75748, "", "");
 setcookie("access_level", "standarduser", 75748, "", "");
 header('location:homepage.php');
 }
+
 else{
 
     ECHO 'Wrong info';
--- login.php
+++ login.php
@@ -4,32 +4,10 @@
 
 <form method="post" action="checkLogIn.php">
 
-    Username:<input type="text" username="username"><br>
-    Password:<input type="password" password="password"><br>
+    Username:<input type="text" name="username"><br>
+    Password:<input type="password" name="password"><br>
     <input type="submit" value="Submit">
 </form>
-
-
-<?php
-
- //   if (isset($_POST['submit'])) {
-   //     if ($username = $_POST["username"] && $password = $_POST["password"]) {
-     //       setcookie("username", $username, 75748, "", "");
-       //     setcookie("access_level", "standarduser", 75748, "", "");
-            //header('Location: homepage.php');
-         //   header('Location: homepage.php');
-          //  exit;
-      //  } else {
-       //     echo "Wrong info";
-
-//        }
-
-
-//}
-//?>
-
-
-
 
 </html>
 
