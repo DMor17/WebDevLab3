a6141e5cb654ca2ce3747c74416b9bb27060df82
Daniel <dannymorison17@Gmail.com>
2015-11-02 13:37:34 +0000

    Initial commit

---
+++ login.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: daniel
+ * Date: 02/11/15
+ * Time: 13:25
+ */
+$username = "Mike";
+$password = "password1";
+
+<form action="welcome.php" method="post">
+    Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
